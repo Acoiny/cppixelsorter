63cdc92d161615166ece979172b238ad67250e82
Acoiny <134412834+Acoiny@users.noreply.github.com>
2026-05-19 12:04:30 +0200

    added config to store app name

---
+++ hdr/Ui/config.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace UI
+{
+namespace Config
+{
+/**
+ * Sets the application name. This is used for things
+ * like finding the config files
+ */
+void set_application_name(const std::string &name);
+
+const std::string_view get_application_name();
+} // namespace Config
+} // namespace UI
